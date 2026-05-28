c380d1c6da9f9f97094d239ec71acbd81eda2bec
jose aguilar <aguilajos5@gmail.com>
2026-05-28 16:45:29 -0600

    mejora de la marca denguemap-hn que coincida con el dominio para homologar la marca

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     >
       <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
         <div className="flex items-center gap-2">
-          <span className="text-xl font-bold tracking-tighter">Denguemap-<span className="text-tech-green">HN</span></span>
+          <span className="text-xl font-bold tracking-tighter" translate="no">denguemap-<span className="text-tech-green">hn</span></span>
         </div>
         
         <div className="hidden items-center gap-8 md:flex">
